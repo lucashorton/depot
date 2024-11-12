e0ee149473bb292d0595b0cd2523212956a23c92
Lucas Horton <lucas@lucashorton.com>
2024-11-12 13:45:55 -0600

    Add tests

--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -29,12 +29,19 @@ class ProductTest < ActiveSupport::TestCase
     product.image.attach(io: File.open("test/fixtures/files/lorem.jpg"), filename: "lorem.jpg", content_type: "image/jpeg")
   end
 
-  test "image url" do 
+  test "image url" do
     product = Product.new(title: "My Book Title", description: "yyy", price: 1)
     product.image.attach(io: File.open("test/fixtures/files/lorem.jpg"), filename: "lorem.jpg", content_type: "image/jpeg")
     assert product.valid?, "image/jpeg must be valid"
     product = Product.new(title: "My Book Title", description: "yyy", price: 1)
     product.image.attach(io: File.open("test/fixtures/files/logo.svg"), filename: "lorem.jpg", content_type: "image/svg+xml")
     assert_not product.valid? "image/svg+xml must be invalid"
+  end
+
+  test "product is not valid without a unique title" do
+    product = Product.new(title: products(:pragprog).title, description: "yyy", price: 1)
+    product.image.attach(io: File.open("test/fixtures/files/lorem.jpg"), filename: "lorem.jpg", content_type: "image/jpeg")
+    assert product.invalid?
+    assert_equal [ "has already been taken" ], product.errors[:title]
   end
 end
